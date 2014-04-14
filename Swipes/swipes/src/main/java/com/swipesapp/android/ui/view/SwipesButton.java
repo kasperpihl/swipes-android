2fedff16f3b237e1004cfa4d9ec4b35e461f32eb
Douglas Drumond <drumond.douglas@gmail.com>
2014-04-14 04:36:52 -0300

    Use custom font for tabs
    Signed-off-by: Douglas Drumond <drumond.douglas@gmail.com>

---
+++ Swipes/swipes/src/main/java/com/swipesapp/android/ui/view/SwipesButton.java
@@ -0,0 +1,39 @@
+package com.swipesapp.android.ui.view;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+public class SwipesButton extends Button {
+    private Context mContext;
+    private static Typeface sTypeface;
+    private static final String FONT_NAME = "41158d87.swipes.ttf";
+
+    public SwipesButton(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public SwipesButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public SwipesButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context);
+    }
+
+    private void init(Context context) {
+        mContext = context;
+        if (sTypeface == null) {
+            synchronized (SwipesButton.class) {
+                if (sTypeface == null) {
+                    sTypeface = Typeface.createFromAsset(mContext.getAssets(), FONT_NAME);
+                }
+            }
+        }
+        this.setTypeface(sTypeface);
+    }
+}
