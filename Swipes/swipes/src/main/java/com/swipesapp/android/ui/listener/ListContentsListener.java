0caabe1c8dd60ca14c8145e7550aa2ce379fdf47
Douglas Drumond <drumond.douglas@gmail.com>
2014-05-21 03:04:25 -0300

    remove creation comment
    Signed-off-by: Douglas Drumond <drumond.douglas@gmail.com>

--- Swipes/swipes/src/main/java/com/swipesapp/android/ui/listener/ListContentsListener.java
+++ Swipes/swipes/src/main/java/com/swipesapp/android/ui/listener/ListContentsListener.java
@@ -1,8 +1,5 @@
 package com.swipesapp.android.ui.listener;
 
-/**
- * Created by douglasdrumond on 4/30/14.
- */
 public interface ListContentsListener {
     void onEmpty();
     void onNotEmpty();
