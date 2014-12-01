ca353c267cec66691168e465b3688ad03c0c8ed3
Stanimir Karoserov <demosten@gmail.com>
2014-12-01 18:37:10 +0200

    adding initial Evernote integration code

---
+++ Swipes/swipes/src/main/java/com/swipesapp/android/evernote/OnEvernoteCallback.java
@@ -0,0 +1,18 @@
+package com.swipesapp.android.evernote;
+
+/**
+ * Created by Stanimir Karoserov on 11/25/14.
+ */
+
+public abstract class OnEvernoteCallback<T> {
+
+    /**
+     * @param data sent to callback when the async operation has completed positively
+     */
+    public abstract void onSuccess(final T data);
+
+    /**
+     * @param exception is the error sent to the callback when the async operation has completed negatively
+     */
+    public abstract void onException(final Exception e);
+}
