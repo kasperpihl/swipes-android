99a7ab660149534827fd26d8f2725b75a0dea8f0
Fernanda Bari <fehbari@gmail.com>
2015-06-11 20:58:17 -0300

    Properly read push data from the Intent as JSON. Silent push fully functional.

--- Swipes/swipes/src/main/java/com/swipesapp/android/sync/receiver/PushReceiver.java
+++ Swipes/swipes/src/main/java/com/swipesapp/android/sync/receiver/PushReceiver.java
@@ -10,6 +10,8 @@ import com.swipesapp.android.sync.service.SyncService;
 import com.swipesapp.android.util.PreferenceUtils;
 import com.swipesapp.android.values.Constants;
 
+import org.json.JSONObject;
+
 /**
  * Custom receiver to handle push messages.
  *
@@ -19,8 +21,14 @@ public class PushReceiver extends ParsePushBroadcastReceiver {
 
     private static final String LOG_TAG = PushReceiver.class.getSimpleName();
 
-    private static final String EXTRA_CONTENT = "content-available";
-    private static final String EXTRA_SYNC_ID = "syncId";
+    public static final String KEY_CHANNELS = "channels";
+    public static final String CHANNEL_DEV = "Development";
+
+    private static final String EXTRA_PAYLOAD = "com.parse.Data";
+
+    private static final String JSON_DATA = "aps";
+    private static final String JSON_CONTENT = "content-available";
+    private static final String JSON_SYNC_ID = "syncId";
 
     private static SyncService sSyncService = SyncService.getInstance();
 
@@ -31,18 +39,27 @@ public class PushReceiver extends ParsePushBroadcastReceiver {
         // Initialize service if needed.
         if (sSyncService == null) sSyncService = SyncService.newInstance(context);
 
-        // Read payload data in the intent.
-        String contentAvailable = intent.getStringExtra(EXTRA_CONTENT);
-        String syncId = intent.getStringExtra(EXTRA_SYNC_ID);
+        try {
+            // Read payload from intent.
+            String payload = intent.getStringExtra(EXTRA_PAYLOAD);
+            JSONObject json = new JSONObject(payload);
 
-        // Determine sync conditions.
-        boolean hasContent = contentAvailable != null && contentAvailable.equals("1");
-        String lastSyncId = PreferenceUtils.readString(PreferenceUtils.LAST_SYNC_ID, context);
-        boolean isDifferentDevice = (syncId != null && !syncId.isEmpty()) && lastSyncId != null && !syncId.equals(lastSyncId);
+            // Read payload data in the JSON.
+            JSONObject data = new JSONObject(json.getString(JSON_DATA));
+            String contentAvailable = data.getString(JSON_CONTENT);
+            String syncId = json.getString(JSON_SYNC_ID);
 
-        if (hasContent && isDifferentDevice) {
-            // Perform background sync.
-            sSyncService.performSync(true, Constants.SYNC_DELAY);
+            // Determine sync conditions.
+            boolean hasContent = contentAvailable != null && contentAvailable.equals("1");
+            String lastSyncId = PreferenceUtils.readString(PreferenceUtils.LAST_SYNC_ID, context);
+            boolean isDifferentDevice = (syncId != null && !syncId.isEmpty()) && lastSyncId != null && !syncId.equals(lastSyncId);
+
+            if (hasContent && isDifferentDevice) {
+                // Perform background sync.
+                sSyncService.performSync(true, Constants.SYNC_DELAY);
+            }
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "Error parsing push payload.", e);
         }
 
         Log.d(LOG_TAG, "Push received");
