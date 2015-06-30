0952132e04ec51d81ac2eb204d0169cbe8456a96
Fernanda Bari <fehbari@gmail.com>
2015-06-30 05:16:14 -0300

    Scrollable tags.

---
+++ Swipes/swipes/src/main/java/com/swipesapp/android/ui/view/SwipesScrollView.java
@@ -0,0 +1,50 @@
+package com.swipesapp.android.ui.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.Display;
+import android.view.WindowManager;
+import android.widget.ScrollView;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * Custom ScrollView that wraps content, while having a maximum height of x% of the screen.
+ *
+ * @author Felipe Bari
+ */
+public class SwipesScrollView extends ScrollView {
+
+    private WeakReference<Context> mContext;
+
+    public SwipesScrollView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public SwipesScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public SwipesScrollView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context);
+    }
+
+    private void init(Context context) {
+        mContext = new WeakReference<>(context);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        WindowManager wm = (WindowManager) mContext.get().getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+
+        int maxHeight = (int) Math.round(display.getHeight() * 0.5);
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(maxHeight, MeasureSpec.AT_MOST);
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+}
